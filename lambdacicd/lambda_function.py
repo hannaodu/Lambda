ad221c451489e8c6a37918ace1d775050195ab56
hanna <tennyalana@yahoo.com>
2025-03-24 15:00:03 +0000

    initial setup

--- lambdacicd/lambda_function.py
+++ lambdacicd/lambda_function.py
@@ -3,6 +3,6 @@ import json
 def lambda_handler(event, context):
     return{
       "statuscode": 200,
-      "body" : json.dumps('Hello updated again LAMBDA!')
+      "body" : json.dumps('Hello updated my LAMBDA!')
     }
 
